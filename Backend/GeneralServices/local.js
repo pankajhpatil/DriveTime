5c7bd758d77ce6aa7ee33a56e45011530c17ebac
Pankaj Patil <pankajhanmant.patil@sjsu.edu>
2020-05-01 16:46:40 -0700

    Revert "Test Backend Working Fine"
    This reverts commit 2c58ea4994c2ebdb565c329da903bfac18e65544.

--- Backend/GeneralServices/local.js
+++
@@ -1,7 +0,0 @@
-const app = require('./src/server.js');
-const port = process.env.PORT || 8000;
-
-// Server
-app.listen(port, () => {
-   console.log(`Listening on: http://localhost:${port}`);
-});
